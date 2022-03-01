29965e1da83ff6eef884106d9183f1c7c0df4130
Akhilesh271988 <tiwa.akhi27@gmail.com>
2022-03-01 08:09:10 +0530

    added more model in ml_model.py

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,20 @@
 print("writing all the model related code here")
+
+
+
+
+print("we added more model")
+
+print("we added more model5")
+
+
+
+
+
+print("we added more model4")
+
+
+
+
+
+print("we added more model3")
